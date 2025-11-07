c219919a774c9c378f7e63c5c94f04be41c3f2a1
ranazcn <118856707+ranazcn@users.noreply.github.com>
2025-11-07 22:09:56 +0300

    models added

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Laravel!';
+});
+
+Route::get('/hello/{name}/info', function ($name) {
+    return "Hello, $name!";
+});
+
+Route::get('/tmp',[TempController::class, 'tmpFunction']);
