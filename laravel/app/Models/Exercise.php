5a7d8564f69071b289b6f56cf9dccdef278f2af5
ranazcn <118856707+ranazcn@users.noreply.github.com>
2025-11-10 23:45:02 +0300

    updated

--- laravel/app/Models/Exercise.php
+++ laravel/app/Models/Exercise.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Activity;
 
 class Exercise extends Model
 {
-    //
+    public function activity()
+    {
+        return $this->belongsTo(Activity::class);
+    }
 }
--- laravel/app/Models/Meal.php
+++ laravel/app/Models/Meal.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
+use App\Models\Food;
 
 class Meal extends Model
 {
